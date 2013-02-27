e42379648b00f490984b7ef21446920b99acd6a2
Cedric Gatay <cedric@gatay.fr>
2013-02-27 14:40:02 +0100

    Bumped to Wicket 6

--- src/main/java/nl/topicus/heroku/wicket/WicketApplication.java
+++ src/main/java/nl/topicus/heroku/wicket/WicketApplication.java
@@ -4,11 +4,10 @@ import org.apache.wicket.protocol.http.WebApplication;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
- * 
+ *
  * @see nl.topicus.heroku.wicket.Start#main(String[])
  */
-public class WicketApplication extends WebApplication
-{    	
+public class WicketApplication extends WebApplication{
 	/**
 	 * @see org.apache.wicket.Application#getHomePage()
 	 */
