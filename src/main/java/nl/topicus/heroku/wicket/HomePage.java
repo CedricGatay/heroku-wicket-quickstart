3598d797729938b77ad7013985b1b012c9e93577
Cedric Gatay <cedric@gatay.fr>
2013-02-28 08:45:34 +0100

    Postgresql first try
    Added postgresql

--- src/main/java/nl/topicus/heroku/wicket/HomePage.java
+++ src/main/java/nl/topicus/heroku/wicket/HomePage.java
@@ -1,15 +1,23 @@
 package nl.topicus.heroku.wicket;
 
+import nl.topicus.heroku.wicket.service.ApplicationNumberService;
 import nl.topicus.heroku.wicket.service.RequestNumberService;
 import nl.topicus.heroku.wicket.service.SessionNumberService;
-import nl.topicus.heroku.wicket.service.ApplicationNumberService;
+import nl.topicus.heroku.wicket.jpa.entities.*;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.inject.Inject;
+import javax.persistence.EntityManager;
 
 public class HomePage extends WebPage {
+    private static final Logger LOGGER = LoggerFactory.getLogger(HomePage.class);
+
+    @Inject
+    EntityManager em;
     @Inject
     RequestNumberService requestScopedService;
     @Inject
@@ -18,7 +26,12 @@ public class HomePage extends WebPage {
     ApplicationNumberService applicationScopedService;
 
     public HomePage(final PageParameters parameters) {
-		add(new Label("request", requestScopedService.getValue()));
+        try {
+            em.find(Toto.class, 1L);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        add(new Label("request", requestScopedService.getValue()));
 		add(new Label("session", sessionScopedService.getValue()));
 		add(new Label("application", applicationScopedService.getValue()));
     }
--- src/main/java/nl/topicus/heroku/wicket/WicketApplication.java
+++ src/main/java/nl/topicus/heroku/wicket/WicketApplication.java
@@ -3,13 +3,18 @@ package nl.topicus.heroku.wicket;
 import org.apache.wicket.cdi.CdiConfiguration;
 import org.apache.wicket.protocol.http.WebApplication;
 import org.jboss.weld.environment.servlet.Listener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.enterprise.inject.spi.BeanManager;
+import java.net.URI;
+import java.net.URISyntaxException;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
  */
 public class WicketApplication extends WebApplication {
+    private static final Logger LOGGER = LoggerFactory.getLogger(WicketApplication.class);
     /**
      * @see org.apache.wicket.Application#getHomePage()
      */
@@ -24,6 +29,22 @@ public class WicketApplication extends WebApplication {
     @Override
     public void init() {
         super.init();
+
+        try {
+            URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+            System.setProperty("hibernate.connection.url", dbUrl);
+            System.setProperty("hibernate.connection.user", username);
+            System.setProperty("hibernate.connection.password", password);
+            LOGGER.info("JDBC URL : {}", System.getProperty("hibernate.connection.url"));
+            LOGGER.info("JDBC User : {}", System.getProperty("hibernate.connection.user"));
+            LOGGER.info("JDBC Password : {}", System.getProperty("hibernate.connection.password"));
+        } catch (URISyntaxException e) {
+            LOGGER.error("Unable to extract database url");
+        }
 
         BeanManager manager = (BeanManager) getServletContext().getAttribute(
                 Listener.BEAN_MANAGER_ATTRIBUTE_NAME);
